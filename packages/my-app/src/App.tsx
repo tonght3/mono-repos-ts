4e6004ce57deefd7ec19d02b4fd597a209ed9213
Huỳnh Thế Tông <tonght3@hdbank.com.vn>
2022-10-06 15:30:29 +0700

    init sourc mono-repo with react-typescript

---
+++ packages/my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { HelloWorld, HDInput } from "my-module";
+
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+      <HDInput />
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/my-module/src/HelloWorld.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export const HelloWorld = () => <div>HelloWorld</div>;
+
+export const HDInput = () => <input placeholder="Vui lòng nhập thông tin TK" />;
