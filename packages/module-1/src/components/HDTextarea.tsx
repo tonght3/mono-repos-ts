bf62c611683c66c9b1285d1f4232a3f37d0d643a
Huỳnh Thế Tông <tonght3@hdbank.com.vn>
2022-10-07 14:44:03 +0700

    update main branch mono-repos

---
+++ packages/module-1/src/components/HDTextarea.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const HDTextarea: React.FC = () => {
+    return <div>
+        <label htmlFor="Tiểu sử">Tiểu sử</label>
+        <textarea rows={5} cols={12} />
+    </div>
+}
---
+++ packages/my-module/src/components/HDTextarea.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const HDTextarea: React.FC = () => {
+    return <textarea rows={5} cols={12} />
+}
